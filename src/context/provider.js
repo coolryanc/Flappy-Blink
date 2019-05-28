911b473daf48d720215d2da3c92f192f65a64a31
RyanC <ryan149347@gmail.com>
2019-05-28 12:27:08 +0800

    feat: setup provider

---
+++ src/context/provider.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import { gameStatusEnum, birdStatusEnum } from 'utils/appEnums';
+
+const DEFAULT_STATE = {
+    game: {
+        status: gameStatusEnum.START,
+    },
+    player: {
+        score: '',
+    },
+    bird: {
+        status: birdStatusEnum.NORMAL,
+    },
+    pipe: {},
+};
+
+export const AppContext = React.createContext(DEFAULT_STATE);
+
+export default class AppProvider extends Component {
+    state = DEFAULT_STATE;
+
+    render() {
+        return (
+            <AppContext.Provider
+                value={{
+                    ...this.state,
+                }}
+            >
+                {this.props.children}
+            </AppContext.Provider>
+        );
+    }
+}
---
+++ src/utils/appEnums.js
@@ -0,0 +1,10 @@
+export const gameStatusEnum = Object.freeze({
+    START: 'START',
+    OVER: 'OVER',
+});
+
+export const birdStatusEnum = Object.freeze({
+    UP: 'UP',
+    NORNAL: 'NORNAL',
+    DOWN: 'DOWN',
+});
